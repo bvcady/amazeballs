e09696adf5773dc22ae31f3897b4aa030feb98f0
Bob Verheij <bobverheij@Bobs-MBP-2.home>
2023-10-30 13:04:46 +0100

    Resolved build issues with wrong imports

--- src/components/lava/LavaSource.tsx
+++ src/components/lava/LavaSource.tsx
@@ -1,5 +1,5 @@
 import { theme } from "@/styles/Global";
-import { CellWrapper } from "../../styles/ASCIIStyled";
+import { CellWrapper } from "@/styles/shared/Shared.styles";
 
 export const LavaSource = () => {
   return (
